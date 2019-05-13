aab77bcbae13edd1d93670e02bd8ec927a61377d
Marvin Zachary <acerzach@gmail.com>
2019-05-13 22:00:58 +0300

    MODIFIED LAYOUTS

--- app/src/main/java/com/marv/cararena/CarsActivity.java
+++ app/src/main/java/com/marv/cararena/CarsActivity.java
@@ -14,11 +14,10 @@ import butterknife.ButterKnife;
 
 public class CarsActivity extends AppCompatActivity {
     private String[] cars = new String[] {"Audi", "Dodge",
-            "Bmw", "Jeep", "Mercedes", "Ferari",
-            "Nissan GTR", "Buggati", "Royce ", "Porche",
+            "Bmw","Nissan GTR", "Buggati",
             "HENNESSEY VENOM", "BUGATTI CHIRON", "KOENIGSEGG CCR",
             "SALEEN S7 TWIN TURBO", "MCLAREN F1"};
-    private String[] price = new String[] {"2.1 million", "9.6 million dolars", "9.6 million dolars", "9.6 million dolars", "9.6 million dolars", "9.1 million dolars", "9.2 million dolars", "8.2 million dolars", "7.2 million dolars", "6.2 million dolars", "5.2 million dolars", "5.8 million dolars", "1.8 million dolars", "4.8 million dolars", "6.8 million dolars", "7.8 million dolars" };
+    private String[] price = new String[] { "9.1 million dolars", "9.2 million dolars", "8.2 million dolars", "7.2 million dolars", "6.2 million dolars", "5.2 million dolars", "5.8 million dolars", "1.8 million dolars", "4.8 million dolars", "6.8 million dolars", "7.8 million dolars" };
 
     @BindView(R.id.modelDisplay) TextView mmodel;
     @BindView(R.id.yearDisplay) TextView myear;
