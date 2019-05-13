7d5592b1ae592be1c152df57aa93a3153a10f7fe
Marvin Zachary <acerzach@gmail.com>
2019-05-13 09:34:23 +0300

    Added MainActivity test

--- app/src/test/java/com/marv/cararena/MainActivityTest.java
+++ app/src/test/java/com/marv/cararena/MainActivityTest.java
@@ -1,24 +1,24 @@
-//package com.marv.cararena;
-//
-//import android.widget.TextView;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.robolectric.Robolectric;
-//import org.robolectric.RobolectricTestRunner;
-//
-//import static junit.framework.TestCase.assertTrue;
-//@RunWith(RobolectricTestRunner.class)
-//public class MainActivityTest {
-//    private MainActivity activity;
-//    @Before
-//    public void setup(){
-//        activity = Robolectric.setupActivity(MainActivity.class);
-//    }
-//    @Test
-//    public void  ValidateTextViewContent(){
-//        TextView appNameTextView =  activity.findViewById(R.id.appNameTextView);
-//        assertTrue("Cars Arena".equals(appNameTextView.getText().toString()));
-//    }
-//}
+package com.marv.cararena;
+
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static junit.framework.TestCase.assertTrue;
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+    @Test
+    public void  ValidateTextViewContent(){
+        TextView appNameTextView =  activity.findViewById(R.id.appNameTextView);
+        assertTrue("Cars Arena".equals(appNameTextView.getText().toString()));
+    }
+}
