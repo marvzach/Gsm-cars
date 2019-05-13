302e0440e4c1d0563b567619151edb0264162905
Marvin Zachary <acerzach@gmail.com>
2019-05-13 09:33:56 +0300

    Added cararenaadapter.java

--- app/src/main/java/com/marv/cararena/cararenaAdapter.java
+++ app/src/main/java/com/marv/cararena/cararenaAdapter.java
@@ -1,28 +1,28 @@
-//package com.marv.cararena;
-//
-//import android.content.Context;
-//import android.widget.ArrayAdapter;
-//
-//public class cararenaAdapter extends ArrayAdapter {
-//    private Context mContext;
-//    private String[] mcars;
-//    private String[] mprice;
-//
-//    public cararenaAdapter(Context mContext, int resource, String[] mcars, String[] mprice) {
-//        super(mContext, resource);
-//        this.mContext = mContext;
-//        this.mcars = mcars;
-//        this.mprice = mprice;
-//    }
-//    @Override
-//    public Object getItem(int position) {
-//        String car = mcars[position];
-//        String price = mprice[position];
-//        return String.format("%s \nThe price is: %s", car, price);
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return mcars.length;
-//    }
-//}
+package com.marv.cararena;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class cararenaAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mcars;
+    private String[] mprice;
+
+    public cararenaAdapter(Context mContext, int resource, String[] mcars, String[] mprice) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mcars = mcars;
+        this.mprice = mprice;
+    }
+    @Override
+    public Object getItem(int position) {
+        String car = mcars[position];
+        String price = mprice[position];
+        return String.format("%s \nThe price is: %s", car, price);
+    }
+
+    @Override
+    public int getCount() {
+        return mcars.length;
+    }
+}
