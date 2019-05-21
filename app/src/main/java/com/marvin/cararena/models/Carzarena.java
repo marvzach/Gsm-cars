7bde8f2a217b846fdc4ce568478d54703e3a15bd
Marvin Zachary <acerzach@gmail.com>
2019-05-21 14:08:24 +0300

    Added objects of Cararena class

--- app/src/main/java/com/marvin/cararena/models/Carzarena.java
+++ app/src/main/java/com/marvin/cararena/models/Carzarena.java
@@ -1,70 +1,70 @@
-//package com.marvin.cararena.models;
-//
-//import org.parceler.Parcel;
-//
-//import java.util.ArrayList;
-//
-//@Parcel
-//public class Carzarena {
-//    String mMake;
-//    String mPhone;
-//    String mWebsite;
-//    String mPrice;
-//    String mPhoto_links;
-//    ArrayList<String> mBuild = new ArrayList<>();
-//    String mLatitude;
-//    String mLongitude;
-//
-//
-//    public Carzarena() {
-//
-//    }
-//
-//    public Carzarena(String make, String model, String website,
-//                     String year, String photo_links, ArrayList<String> build,
-//                     String latitude, String longitude) {
-//        this.mMake = make;
-//        this.mPhone = model;
-//        this.mWebsite = website;
-//        this.mPrice = year;
-//        this.mPhoto_links = photo_links;
-//        this.mBuild = build;
-//        this.mLatitude = latitude;
-//        this.mLongitude = longitude;
-//
-//    }
-//
-//    public String getMake() {
-//        return mMake;
-//    }
-//
-//    public String getPhone() {
-//        return mPhone;
-//    }
-//
-//    public String getWebsite() {
-//        return  mWebsite;
-//    }
-//
-//    public String getPrice() {
-//        return mPrice;
-//    }
-//
-//    public String getPhoto_links(){
-//        return mPhoto_links;
-//    }
-//
-//    public ArrayList<String> getBuild() {
-//        return mBuild;
-//    }
-//
-//    public String getLatitude() {
-//        return mLatitude;
-//    }
-//
-//    public String getLongitude() {
-//        return mLongitude;
-//    }
-//
-//
-//}
+package com.marvin.cararena.models;
+
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+
+@Parcel
+public class Carzarena {
+    String mMake;
+    String mPhone;
+    String mWebsite;
+    String mPrice;
+    String mPhoto_links;
+    ArrayList<String> mBuild = new ArrayList<>();
+    String mLatitude;
+    String mLongitude;
+
+
+    public Carzarena() {
+
+    }
+
+    public Carzarena(String make, String model, String website,
+                     String year, String photo_links, ArrayList<String> build,
+                     String latitude, String longitude) {
+        this.mMake = make;
+        this.mPhone = model;
+        this.mWebsite = website;
+        this.mPrice = year;
+        this.mPhoto_links = photo_links;
+        this.mBuild = build;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+
+    }
+
+    public String getMake() {
+        return mMake;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public String getWebsite() {
+        return  mWebsite;
+    }
+
+    public String getPrice() {
+        return mPrice;
+    }
+
+    public String getPhoto_links(){
+        return mPhoto_links;
+    }
+
+    public ArrayList<String> getBuild() {
+        return mBuild;
+    }
+
+    public String getLatitude() {
+        return mLatitude;
+    }
+
+    public String getLongitude() {
+        return mLongitude;
+    }
+
+
+}
