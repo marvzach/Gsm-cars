852ed4dc1c1a8634203d9be0f9cc6fe39dc21ff9
Marvin Zachary <acerzach@gmail.com>
2019-05-21 14:10:22 +0300

    Added and arraylist

--- app/src/main/java/com/marvin/cararena/adapters/CararenaPagerAdapter.java
+++ app/src/main/java/com/marvin/cararena/adapters/CararenaPagerAdapter.java
@@ -1,17 +1,17 @@
-//package com.marvin.cararena.adapters;
-//
-//import com.marvin.cararena.models.Carzarena;
-//import com.marvin.cararena.ui.CararenaDetailFragment;
-//
-//import java.util.ArrayList;
-//
-//import androidx.fragment.app.Fragment;
-//import androidx.fragment.app.FragmentManager;
-//import androidx.fragment.app.FragmentPagerAdapter;
-//
-//public class CararenaPagerAdapter extends FragmentPagerAdapter {
-//    private ArrayList<Carzarena> mCarzarenas;
-//
+package com.marvin.cararena.adapters;
+
+import com.marvin.cararena.models.Carzarena;
+import com.marvin.cararena.ui.CararenaDetailFragment;
+
+import java.util.ArrayList;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class CararenaPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Carzarena> mCarzarenas;
+
 //    public CararenaPagerAdapter(FragmentManager fm, ArrayList<Carzarena> carzarenas) {
 //        super(fm);
 //        mCarzarenas = carzarenas;
