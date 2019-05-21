2d3ce66d9530449c33d303da1813f06614300595
Marvin Zachary <acerzach@gmail.com>
2019-05-21 14:10:52 +0300

    Added CararenaListAdapter

--- app/src/main/java/com/marvin/cararena/adapters/CararenaPagerAdapter.java
+++ app/src/main/java/com/marvin/cararena/adapters/CararenaPagerAdapter.java
@@ -12,23 +12,23 @@ import androidx.fragment.app.FragmentPagerAdapter;
 public class CararenaPagerAdapter extends FragmentPagerAdapter {
     private ArrayList<Carzarena> mCarzarenas;
 
-//    public CararenaPagerAdapter(FragmentManager fm, ArrayList<Carzarena> carzarenas) {
-//        super(fm);
-//        mCarzarenas = carzarenas;
-//    }
-//
-//    @Override
-//    public Fragment getItem(int position) {
-//        return CararenaDetailFragment.newInstance(mCarzarenas.get(position));
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return mCarzarenas.size();
-//    }
-//
-//    @Override
-//    public CharSequence getPageTitle(int position) {
-//        return mCarzarenas.get(position).getMake();
-//    }
-//}
+    public CararenaPagerAdapter(FragmentManager fm, ArrayList<Carzarena> carzarenas) {
+        super(fm);
+        mCarzarenas = carzarenas;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return CararenaDetailFragment.newInstance(mCarzarenas.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mCarzarenas.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mCarzarenas.get(position).getMake();
+    }
+}
