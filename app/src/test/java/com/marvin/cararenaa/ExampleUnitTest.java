e6547678beaebfd6f25e9d3b731e71b75c110893
Marvin Zachary <acerzach@gmail.com>
2019-06-03 15:00:01 +0300

    Added searchview

--- app/src/test/java/com/marvin/cararenaa/ExampleUnitTest.java
+++ app/src/test/java/com/marvin/cararenaa/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.marvin.cararenaa.car;
+package com.marvin.cararenaa;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
